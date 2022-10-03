510059b5a7b7ab1c22734237719035406d9ec7da
Ajayi Olamide <prodbyola@gmail.com>
2022-10-03 11:41:39 +0100

    removed builds

--- build/interface.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MOUSE_POSITIONS = void 0;
-exports.MOUSE_POSITIONS = ['top', 'bottom', 'left', 'right', 'top-right', 'top-left', 'bottom-right', 'bottom-left'];
